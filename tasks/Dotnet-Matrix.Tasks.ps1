8155ceb524fba9dc511560adf8a13fc9775637cd
Steffen <skampmann@munichre.com>
2022-05-12 07:57:20 +0200

    fixed path

--- tasks/Dotnet-Matrix.Tasks.ps1
+++ tasks/Dotnet-Matrix.Tasks.ps1
@@ -3,7 +3,7 @@
 task UpdateBuildTasks {
     Invoke-WebRequest `
         -Uri 'https://raw.githubusercontent.com/abbgrade/PsBuildTasks/main/DotNet/Build.Tasks.ps1' `
-        -OutFile "$PSScriptRoot\tasks\Build.Tasks.ps1"
+        -OutFile "$PSScriptRoot\Build.Tasks.ps1"
 }
 
 #endregion
