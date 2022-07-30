5f1bfa704d63f1650ac9bf0ee70b0b40e1b3aa14
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-07-30 08:46:35 +0200

    updated dependencies

--- .build.ps1
+++ .build.ps1
@@ -3,3 +3,7 @@ $ModuleName = 'PsBuildTasks'
 
 . $PSScriptRoot/tasks/PowerShell-Matrix.Tasks.ps1
 . $PSScriptRoot/tasks/Build.Tasks.ps1
+
+task InstallBuildDependencies {}
+task InstallTestDependencies {}
+task InstallReleaseDependencies {}
